63a3929aa1bec82c44caee8654ece7986da4ce7e
Ralimist <28601907+Teoxoy@users.noreply.github.com>
2018-02-17 02:47:10 +0100

    new version

--- bin/pixi-picture.d.ts
+++ bin/pixi-picture.d.ts
@@ -1,33 +1,33 @@
 /// <reference types="pixi.js" />
-declare module PIXI.extras {
+declare module pixi_picture {
     class PictureShader extends PIXI.Shader {
-        tempQuad: Quad;
+        tempQuad: PIXI.Quad;
         tilingMode: number;
         static blendVert: string;
         constructor(gl: WebGLRenderingContext, vert: string, frag: string, tilingMode: number);
     }
 }
-declare module PIXI.extras {
+declare module pixi_picture {
     class HardLightShader extends PictureShader {
         constructor(gl: WebGLRenderingContext, tilingMode: number);
     }
 }
-declare module PIXI.extras {
+declare module pixi_picture {
     function mapFilterBlendModesToPixi(gl: WebGLRenderingContext, array?: Array<Array<PictureShader>>): Array<Array<PictureShader>>;
 }
-declare module PIXI.extras {
+declare module pixi_picture {
     class NormalShader extends PictureShader {
         constructor(gl: WebGLRenderingContext, tilingMode: number);
     }
 }
-declare module PIXI.extras {
+declare module pixi_picture {
     class OverlayShader extends PictureShader {
         constructor(gl: WebGLRenderingContext, tilingMode: number);
     }
 }
-declare module PIXI.extras {
-    class PictureRenderer extends ObjectRenderer {
-        constructor(renderer: WebGLRenderer);
+declare module pixi_picture {
+    class PictureRenderer extends PIXI.ObjectRenderer {
+        constructor(renderer: PIXI.WebGLRenderer);
         drawModes: Array<Array<PictureShader>>;
         normalShader: Array<PictureShader>;
         _tempClamp: Float32Array;
@@ -42,24 +42,24 @@ declare module PIXI.extras {
         onContextChange(): void;
         start(): void;
         flush(): void;
-        _getRenderTexture(minWidth: number, minHeight: number): BaseRenderTexture;
+        _getRenderTexture(minWidth: number, minHeight: number): PIXI.BaseRenderTexture;
         _getBuf(size: number): Float32Array;
-        render(sprite: Sprite): void;
-        _renderNormal(sprite: Sprite, shader: PictureShader): void;
-        _renderBlend(sprite: Sprite, shader: PictureShader): void;
-        _renderInner(sprite: Sprite, shader: PictureShader): void;
-        _renderWithShader(ts: TilingSprite, isSimple: boolean, shader: PictureShader): void;
-        _renderSprite(sprite: Sprite, shader: PictureShader): void;
-        _isSimpleSprite(ts: Sprite): boolean;
+        render(sprite: PIXI.Sprite): void;
+        _renderNormal(sprite: PIXI.Sprite, shader: PictureShader): void;
+        _renderBlend(sprite: PIXI.Sprite, shader: PictureShader): void;
+        _renderInner(sprite: PIXI.Sprite, shader: PictureShader): void;
+        _renderWithShader(ts: PIXI.extras.TilingSprite, isSimple: boolean, shader: PictureShader): void;
+        _renderSprite(sprite: PIXI.Sprite, shader: PictureShader): void;
+        _isSimpleSprite(ts: PIXI.Sprite): boolean;
     }
 }
-declare module PIXI.extras {
-    class PictureSprite extends Sprite {
-        constructor(texture: Texture);
+declare module pixi_picture {
+    class PictureSprite extends PIXI.Sprite {
+        constructor(texture: PIXI.Texture);
     }
 }
-declare module PIXI.extras {
-    class PictureTilingSprite extends TilingSprite {
-        constructor(texture: Texture);
+declare module pixi_picture {
+    class PictureTilingSprite extends PIXI.extras.TilingSprite {
+        constructor(texture: PIXI.Texture);
     }
 }
